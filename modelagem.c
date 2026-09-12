aca091d780975d7a34feffd34de554984166734a
iagoBc <iagobariuka@gmail.com>
2026-09-12 19:05:28 -0300

    Adiciona restrição para garantir que a energia que entra e sai das centrais atenda à demanda

--- modelagem.c
+++ modelagem.c
@@ -53,5 +53,49 @@ void restricoes(int hidros_num, int vazao_rio, hidro *hidros, central *centrais,
         }
     }
 
+    for(int i=0; i<central_num; i++){   // sum(eij) - sum(eji) = Di -> o que entra e sai da central deve ser igual a demanda da central
+        for(int j=0; j<hidros_num; j++){
+            for(int k=0; k<hidros[j].num_arcos; k++){
+                if(hidros[j].arcos[k].indice_c == i+1){
+                    printf("+e%d_%d ", j+1, hidros[j].arcos[k].indice_c);
+                }
+            }
+        }
 
+        for(int j=0; j<central_num; j++){
+            if(j == i) continue;
+            for(int k=0; k<centrais[j].num_arcos; k++){
+                if(centrais[j].arcos[k].indice_c == i+1){
+                    printf("+e%d_%d ", hidros_num+j+1, centrais[j].arcos[k].indice_c);
+                }
+            }
+        }
+
+        for(int j=0; j<centrais[i].num_arcos; j++){ // energia que sai da central
+            if(j == centrais[i].num_arcos-1){
+                printf("-e%d_%d ", hidros_num+i+1, centrais[i].arcos[j].indice_c);
+                continue;
+            }
+            printf("-e%d_%d ", hidros_num+i+1, centrais[i].arcos[j].indice_c);
+        }
+
+        printf("= %d;\n", centrais[i].demanda);
+    }
+
+//    for(int i=0; i<hidros_num; i++){    // Vi >= 0 -> energia produzida pela //hidreletrica deve ser maior ou igual a 0
+//        printf("V%d >= 0;\n", i+1);
+//    }
+//
+//    for(int i=0; i<hidros_num; i++){    // eij >= 0 -> energia que sai da //hidreletrica deve ser maior ou igual a 0
+//        for(int j=0; j<hidros[i].num_arcos; j++){
+//            printf("e%d_%d >= 0;\n", i+1, hidros[i].arcos[j].indice_c);
+//        }
+//    }     
+//                                                                               
+//    for(int i=0; i<central_num; i++){   // eij >= 0 -> energia que sai da central //deve ser maior ou igual a 0
+//        for(int j=0; j<centrais[i].num_arcos; j++){
+//            printf("e%d_%d >= 0;\n", hidros_num+i+1, centrais[i].arcos[j].indice_c);
+//        }
+//    }                                                                                     
 }
+
