dbda8964fcaf66f045bedd4f912770afeaf74587
iagoBc <iagobariuka@gmail.com>
2026-09-13 16:38:03 -0300

    Refatora as funções funcao_obj e restricoes para utilizar tipos unsigned int e inclui o parâmetro arcos_num

--- modelagem.c
+++ modelagem.c
@@ -1,85 +1,59 @@
 #include "modelagem.h"
+typedef unsigned int uint;
 
-
-void funcao_obj(int hidros_num, Hidro *hidros, Central *centrais, int central_num){
+// Função que imprime a função objetivo do modelo de programação linear
+// Sum(Ci x Vi) + Sum(cij x eij)
+// Ci = custo de producao da hidreletrica, Vi = vasao de agua da hidreletrica, cij = custo de transmissao do arco, eij = energia transmitida pelo arco
+void funcao_obj(uint hidros_num, uint central_num, uint arcos_num, Hidre *hidros, Central *centrais, Arco *arcos){
     printf("min : ");
-    for(int i=0; i<hidros_num; i++){
-        printf("%d*V%d + ", hidros[i].custo_producao, i+1);
+    for(uint i=0; i<hidros_num; i++){            // Sum(Ci x Vi)
+        printf("%d*V%d + ", hidros[i].custo, i+1);
     }
 
-    for(int i =0; i < hidros_num; i++){
-        for(int j = 0; j < hidros[i].num_arcos; j++){
-            printf("%d*e%d_%d + ", hidros[i].arcos[j].custo, i+1, hidros[i].arcos[j].indice_c);
+    for(uint i =0; i < arcos_num; i++){          // Sum(cij x eij) | cij
+        if(i == arcos_num-1){
+            printf("%d*e%d_%d;\n", arcos[i].custo, i+1, arcos[i].destino);
+            continue;
         }
+            printf("%d*e%d_%d + ", arcos[i].custo, i+1, arcos[i].destino);
     }
-
-   for(int i =0; i < central_num; i++){
-       for(int j = 0; j < centrais[i].num_arcos; j++){
-           if(j == centrais[i].num_arcos-1){
-               printf("%d*e%d_%d", centrais[i].arcos[j].custo, hidros_num+i+1, centrais[i].arcos[j].indice_c);
-               continue;
-           }
-           printf("%d*e%d_%d + ", centrais[i].arcos[j].custo, hidros_num+i+1, centrais[i].arcos[j].indice_c);
-       }
-   }
-    printf(";\n");
 }
 
 
-
-void restricoes(int hidros_num, int vazao_rio, Hidro *hidros, Central *centrais, int central_num){
-    for(int i=0; i<hidros_num; i++){    
-        printf("%d*V%d <= %d;\n", hidros[i].eficiencia, i+1, hidros[i].capacidade);         // Fi x Vi <= Mi -> producao de energia da hidreletrica <= capacidade maxima da hidreletrica
+// Função que imprime as restrições do modelo de programação linear
+// Fi x Vi <= Mi | producao de energia da hidreletrica <= capacidade maxima da hidreletrica
+// Vi <= R | vazao de agua da hidreletrica <= vazao do rio
+// eij <= wij | energia transmitida pelo arco <= capacidade do arco
+// sum(eij) = Fi x Vi | o que sai da hidreletrica
+// sum(eij) - sum(eji) >= Di | o que chega na central - o que sai da central = a demanda da central
+// Vi, eij >= 0 | energia produzida pela hidreletrica e energia transmitida pelo arco devem ser maior ou igual a 0
+void restricoes(uint hidros_num, uint central_num, uint arcos_num, uint vazao_rio, Hidre *hidros, Central *centrais, Arco *arcos){
+    for(uint i=0; i<hidros_num; i++){    
+        printf("%d*V%d <= %d;\n", hidros[i].eficiencia, i+1, hidros[i].capacidade);         // Fi x Vi <= Mi | producao de energia da hidreletrica <= capacidade maxima da hidreletrica
         
-        printf("V%d <= %d;\n", i+1, vazao_rio);         // Vi <= R -> vazao de agua da hidreletrica <= vazao do rio
+        printf("V%d <= %d;\n", i+1, vazao_rio);         // Vi <= R | vazao de agua da hidreletrica <= vazao do rio
         
-        printf("V%d >= 0;\n", i+1);         // Vi >= 0 -> energia produzida pela hidreletrica deve ser maior ou igual a 0
-
-        for(int j=0; j<hidros[i].num_arcos; j++){ 
-            printf("e%d_%d <= %d;\n", i+1, hidros[i].arcos[j].indice_c, hidros[i].arcos[j].capacidade);         // eij <= wij -> energia que sai da hidreletrica <= capacidade do arco
-
-            printf("e%d_%d = %d*V%d;\n", i+1, hidros[i].arcos[j].indice_c, hidros[i].eficiencia, i+1);          // sum(eij) = Fi x Vi -> o que sai da hidreletrica
-
-            printf("e%d_%d >= 0;\n", i+1, hidros[i].arcos[j].indice_c);         // eij >= 0 -> energia que sai da hidreletrica deve ser maior ou igual a 0
-        }
+        printf("V%d >= 0;\n", i+1);         // Vi >= 0 | energia produzida pela hidreletrica deve ser maior ou igual a 0
     }
 
-    for(int i=0; i<central_num; i++){   
-        for(int j=0; j<centrais[i].num_arcos; j++){
-            printf("e%d_%d <= %d;\n", hidros_num+i+1, centrais[i].arcos[j].indice_c, centrais[i].arcos[j].capacidade);          // eij <= wij -> energia que sai da central <= capacidade do arco
+    for(uint i=0; i < arcos_num; i++){ 
+            printf("e%d_%d <= %d;\n", i+1, arcos[i].destino, arcos[i].capacidade);         // eij <= wij | energia transmitida pelo arco <= capacidade do arco
+ 
+            if(arcos[i].origem <= hidros_num) printf("e%d_%d = %d*V%d;\n", i+1, arcos[i].destino, hidros[i].eficiencia, i+1);          // sum(eij) = Fi x Vi | o que sai da hidreletrica
 
-            printf("e%d_%d >= 0;\n", hidros_num+i+1, centrais[i].arcos[j].indice_c);            // eij >= 0 -> energia que sai da central deve ser maior ou igual a 0
+            printf("e%d_%d >= 0;\n", i+1, arcos[i].destino);         // eij >= 0 | energia transmitida pelo arco deve ser maior ou igual a 0
+    }                                                                      
+    
+    for(uint i=0; i < central_num; i++){
+        for(uint j=0; j < arcos_num; j++){
+            if(arcos[j].destino == i+1) printf("+e%d_%d ", j+1, arcos[j].destino);   
         }
+
+        for(uint j=0; j < arcos_num; j++){
+            if(arcos[j].origem == hidros_num+i+1) printf("-e%d_%d ", j+1, arcos[j].destino);   
+        }
+
+        printf("= %d;\n", centrais[i].demanda);         // sum(eij) - sum(eji) >= Di | o que chega na central - o que sai da central = a demanda da central
     }
-
-    for(int i=0; i<central_num; i++){           // sum(eij) - sum(eji) = Di -> o que entra e sai da central deve ser igual a demanda da central
-        for(int j=0; j<hidros_num; j++){            // energia que entra na central pelas hidreletricas
-            for(int k=0; k<hidros[j].num_arcos; k++){
-                if(hidros[j].arcos[k].indice_c == i+1){
-                    printf("+e%d_%d ", j+1, i+1);
-                }
-            }
-        }
-
-        for(int j=0; j<central_num; j++){           // energia que entra na central pelas outras centrais
-            if(j == i) continue;
-            for(int k=0; k<centrais[j].num_arcos; k++){
-                if(centrais[j].arcos[k].indice_c == i+1){
-                    printf("+e%d_%d ", hidros_num+j+1, i+1);
-                }
-            }
-        }
-
-        for(int j=0; j<centrais[i].num_arcos; j++){     // energia que sai da central
-            if(j == centrais[i].num_arcos-1){
-                printf("-e%d_%d ", hidros_num+i+1, centrais[i].arcos[j].indice_c);
-                continue;
-            }
-            printf("-e%d_%d ", hidros_num+i+1, centrais[i].arcos[j].indice_c);
-        }
-
-        printf("= %d;\n", centrais[i].demanda);
-    }
-                                                                                                                                                              
 }
 
--- modelagem.h
+++ modelagem.h
@@ -3,10 +3,10 @@
 
 #include "hidreletrica.h"
 #include "central.h"
+#include "arco.h"
 
-
-void funcao_obj(int hidros_num, Hidro *hidros, Central *centrais, int central_num);
-void restricoes(int hidros_num, int vazao_rio, Hidro *hidros, Central *centrais, int central_num);
+void funcao_obj(unsigned int hidros_num, unsigned int central_num, unsigned int arcos_num, Hidre *hidros, Central *centrais, Arco *arcos);
+void restricoes(unsigned int hidros_num, unsigned int central_num, unsigned int arcos_num, unsigned int vazao_rio, Hidre *hidros, Central *centrais, Arco *arcos);
 
 
 #endif
