33b11e6331761011fa91d5ee9dd13ea0bdefa736
iagoBc <iagobariuka@gmail.com>
2026-09-14 19:57:29 -0300

    corrige a impressão das variáveis na função funcao_obj e ajusta a formatação das restrições na função restricoes para melhorar a legibilidade do código

--- codigos/modelagem.c
+++ codigos/modelagem.c
@@ -11,12 +11,12 @@ void funcao_obj(uint hidros_num, uint central_num, uint arcos_num, Hidre *hidros
         printf("%d*V%d + ", hidros[i].custo, i+1);
     }
 
-    for(uint i =0; i < arcos_num; i++){          // Sum(cij x eij) | cij
+    for(uint i =0; i < arcos_num; i++){          // Sum(cij x eij) 
         if(i == arcos_num-1){
-            printf("%d*e%d_%d;\n", arcos[i].custo, i+1, arcos[i].destino);
+            printf("%d*e%d_%d;\n", arcos[i].custo, arcos[i].origem, arcos[i].destino);
             continue;
         }
-            printf("%d*e%d_%d + ", arcos[i].custo, i+1, arcos[i].destino);
+            printf("%d*e%d_%d + ", arcos[i].custo, arcos[i].origem, arcos[i].destino);
     }
 }
 
@@ -29,37 +29,41 @@ void funcao_obj(uint hidros_num, uint central_num, uint arcos_num, Hidre *hidros
 // sum(eij) - sum(e(i+h)j) = Di | o que chega na central - o que sai da central = a demanda da central
 // Vi, eij >= 0 | energia produzida pela hidreletrica e energia transmitida pelo arco devem ser maior ou igual a 0
 void restricoes(uint hidros_num, uint central_num, uint arcos_num, uint vazao_rio, Hidre *hidros, Central *centrais, Arco *arcos){
-    for(uint i=0; i<hidros_num; i++){    
-        printf("%d*V%d <= %d;\n", hidros[i].eficiencia, i+1, hidros[i].capacidade);             // Fi x Vi <= Mi
+    for(uint i=0; i<hidros_num; i++) printf("%d*V%d <= %d;\n", hidros[i].eficiencia, i+1, hidros[i].capacidade);             // Fi x Vi <= Mi
+    
+    for(uint i=0; i<hidros_num; i++) printf("V%d <= %d;\n", i+1, vazao_rio);                                                 // Vi <= R
 
-        printf("V%d <= %d;\n", i+1, vazao_rio);                                                 // Vi <= R
-
-        printf("V%d >= 0;\n", i+1);                                                             // Vi >= 0 
-
+    for(uint i=0; i<arcos_num; i++) printf("e%d_%d <= %d;\n", arcos[i].origem, arcos[i].destino, arcos[i].capacidade);              // eij <= wij 
+                                                               
+    for(uint i=0; i<hidros_num; i++){
+        int primeiro = 1;
         for(uint j=0; j<arcos_num; j++){                                                        // sum(eij) = Fi x Vi             
             if(arcos[j].origem == i+1){
-                printf("+e%d_%d", j+1, arcos[j].destino);
+                if(primeiro == 1) printf("e%d_%d", arcos[j].origem, arcos[j].destino);
+                else printf(" + e%d_%d", arcos[j].origem, arcos[j].destino);
+                primeiro = 0;
                 }
             }
             printf(" = %d*V%d;\n", hidros[i].eficiencia, i+1);    
         }
 
-    for(uint i=0; i<arcos_num; i++){ 
-            printf("e%d_%d <= %d;\n", i+1, arcos[i].destino, arcos[i].capacidade);              // eij <= wij 
- 
-            printf("e%d_%d >= 0;\n", i+1, arcos[i].destino);                                    // eij >= 0 
-    }                                                                      
-    
     for(uint i=0; i<central_num; i++){
+        int primeiro = 1;
         for(uint j=0; j<arcos_num; j++){
-            if(arcos[j].destino == i+1) printf("+e%d_%d ", j+1, arcos[j].destino);              // +sum(eij)
+            if(arcos[j].destino == i+1){
+                if(primeiro == 1) printf("e%d_%d ", arcos[j].origem, arcos[j].destino);
+                else printf("+ e%d_%d ", arcos[j].origem, arcos[j].destino);
+                primeiro = 0;
+            }
         }
-
+        
         for(uint j=0; j<arcos_num; j++){
-            if(arcos[j].origem == hidros_num+i+1) printf("-e%d_%d ", j+1, arcos[j].destino);    // -sum(e(i+h)j)   
+            if(arcos[j].origem == hidros_num+i+1) printf("- e%d_%d ", arcos[j].origem, arcos[j].destino);    // -sum(e(i+h)j)   
         }
 
         printf("= %d;\n", centrais[i].demanda);                                                 // sum(eij) - sum(e(i+h)j) >= Di
     }
-}
 
+    for(uint i = 0; i<hidros_num; i++)printf("V%d >= 1;\n", i+1);                                 // Vi > 0
+    for(uint i = 0; i<arcos_num; i++) printf("e%d_%d >= 1;\n", arcos[i].origem, arcos[i].destino);            // eij > 0 
+}
